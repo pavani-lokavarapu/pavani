cc654d0bf60a533f426a82ff355ec5dc27616446
acer <acer@192.168.0.100>
2022-06-25 16:56:36 +0530

    true or false

---
+++ CG training/src/decistst.java
@@ -0,0 +1,17 @@
+
+public class decistst {
+
+	public static void main(String[] args) {
+		int x = 15, y = 20;
+		 if(x >= y)
+		 {
+		 System.out.println("true");
+		 }
+		 else
+		 {
+		 System.out.println("false");
+
+	}
+
+}
+}
