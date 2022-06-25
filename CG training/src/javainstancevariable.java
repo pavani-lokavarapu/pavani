e89488125bb5ddec755854b39f1691d818f1788e
acer <acer@192.168.0.100>
2022-06-25 13:04:20 +0530

    instancevariable

---
+++ CG training/src/javainstancevariable.java
@@ -0,0 +1,11 @@
+
+public class javainstancevariable {
+	int i=25;
+
+	public static void main(String[] args) {
+    javainstancevariable obj = new javainstancevariable();
+    System.out.println("the value of instance variable is "  + obj.i);
+
+	}
+
+}
