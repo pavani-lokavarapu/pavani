7c0545d5f6afcd567690740d9398310a50563453
acer <acer@192.168.0.100>
2022-06-25 17:08:51 +0530

    true or false

---
+++ CG training/src/controlstat.java
@@ -0,0 +1,19 @@
+
+public class controlstat {
+
+	public static void main(String[] args) {
+		int x = 15; int y = 20;
+
+		 if(!(x < y) || (x == y))
+		 {
+		 System.out.println("Condition is true");
+		 }
+		 else
+		 {
+		 System.out.println("Condition is false");
+		}
+	}
+}
+
+		
+		
