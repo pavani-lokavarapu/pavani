0e8ebd3a13eb23efbf87ed6a682872746eca484f
acer <acer@192.168.0.100>
2022-06-25 15:34:53 +0530

    comparisonoperator

---
+++ CG training/src/comparisonoperator.java
@@ -0,0 +1,15 @@
+
+public class comparisonoperator {
+
+	public static void main(String[] args) {
+int	a=25;
+int b=15;
+
+a= (b==1) ? 25:50;
+System.out.println("value of a is : " + a);
+boolean f=a>=b;
+System.out.println(f);
+
+	}
+
+}
