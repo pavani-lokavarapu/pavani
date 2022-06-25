659511eb92df6b48d8d974c44fcc03b2f592fe55
acer <acer@192.168.0.100>
2022-06-25 14:36:42 +0530

    unary operator

---
+++ CG training/src/unaryoperator.java
@@ -0,0 +1,17 @@
+
+public class unaryoperator {
+
+	public static void main(String[] args) {
+	int	a=3;
+	int b=4;
+	int	c=5;
+	int	d=6;
+		int e=a++;
+		System.out.println(e);
+   e=a;
+   System.out.println(e);
+  c++;
+   System.out.println(c); 
+	}
+
+}
