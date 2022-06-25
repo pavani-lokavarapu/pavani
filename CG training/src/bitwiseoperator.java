f2a52b6cda9b6f9f040f160656d46d76045568a5
acer <acer@192.168.0.100>
2022-06-25 14:57:03 +0530

    bitwiseoperator

---
+++ CG training/src/bitwiseoperator.java
@@ -0,0 +1,18 @@
+
+public class bitwiseoperator {
+
+	public static void main(String[] args) {
+  int a=70;
+  int b=40;
+  int result;
+   
+  result= a & b;
+  System.out.println(result);
+  result=a ^ b;
+  System.out.println(result);
+  result=a | b;
+  System.out.println(result);
+
+	}
+
+}
