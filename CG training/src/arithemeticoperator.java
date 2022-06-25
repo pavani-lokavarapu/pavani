2e806c1d77be5c2d1f428d494da43edf84a7984b
acer <acer@192.168.0.100>
2022-06-25 16:48:28 +0530

    values of result

---
+++ CG training/src/arithemeticoperator.java
@@ -0,0 +1,12 @@
+
+public class arithemeticoperator {
+
+	public static void main(String[] args) {
+		int x = 40, y=60;
+		System.out.println(x+y); 
+		System.out.println(--x); 
+		System.out.println(++y); 
+
+	}
+
+}
