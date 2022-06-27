35245ff21daf8b34407dffa2d0327e5fa48afa33
acer <acer@192.168.0.100>
2022-06-27 11:59:31 +0530

    0 or 1

---
+++ CG training/src/ternaryoperator.java
@@ -0,0 +1,13 @@
+
+public class ternaryoperator {
+
+	public static void main(String[] args) {
+		int x = 21;
+		x = (20 == x) ? 1 : 0; 
+		System.out.println(x);
+int y=4;
+y= ( 4 == y) ? 1:0;
+System.out.println(y);
+	}
+
+}
