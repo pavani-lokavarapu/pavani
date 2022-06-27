f6390e698822d6d3c79038b013269960f0a7ef96
acer <acer@192.168.0.100>
2022-06-27 13:13:35 +0530

    looping statements

---
+++ CG training/src/loopingstat.java
@@ -0,0 +1,27 @@
+
+public class loopingstat {
+
+	public static void main(String[] args) {
+		for(int i=1,j=1; i<10||j<10;i++,j++)
+	 { 
+	 j++;
+	 System.out.println("i="+i+" and j="+j); 
+	 }
+	 // while loop
+		int i=1,j=1;
+	 while(i<10 || j<10)
+	 { 
+	 i++; j++;
+	 System.out.println("i="+i+" and j="+j); 
+	 }
+	// Do while loop
+
+	do {
+	i++;
+	j++;
+	System.out.println("i=" + i + " and j=" + j);
+	 } while (i < 10 || j < 10);
+
+	}
+
+}
