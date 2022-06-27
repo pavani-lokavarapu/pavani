74ef7afeb69942dfe3cf1170905ecf894ec74002
acer <acer@192.168.0.100>
2022-06-27 12:49:09 +0530

    for loop

---
+++ CG training/src/loopstatements.java
@@ -0,0 +1,13 @@
+
+public class loopstatements {
+
+	public static void main(String[] args) {
+		for(int i = 10; i<=100; i++) 
+		{
+		System.out.print("Value of i:");
+		System.out.println(i);
+
+	}
+
+}
+}
