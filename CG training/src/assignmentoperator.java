50ec142b212c8d1568be115bedc2f2bd411a7914
acer <acer@192.168.0.100>
2022-06-27 12:06:58 +0530

    assignment operators

---
+++ CG training/src/assignmentoperator.java
@@ -0,0 +1,19 @@
+
+public class assignmentoperator {
+
+	public static void main(String[] args) {
+		int x = 5;
+		x += 4; // x = x + 5
+		System.out.println(x);
+        x -= 6; // x = x - 5
+        System.out.println(x);
+		x *= 7; // x = x * 5
+		System.out.println(x);
+		x /= 8; // x = x / 4
+		System.out.println(x);
+		x %= 5; // x = x % 5
+		System.out.println(x);
+
+	}
+
+}
