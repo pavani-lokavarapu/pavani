cf978d5fa9bf8b60c31bbc525557234a2549df6b
acer <acer@192.168.0.100>
2022-06-27 12:44:09 +0530

    string switchcase

---
+++ CG training/src/stringswitchcase.java
@@ -0,0 +1,24 @@
+
+public class stringswitchcase {
+
+	public static void main(String[] args) {
+		String name = "author";
+		switch (name.toLowerCase()) 
+		{
+		case "author":
+		System.out.println("aravind");
+		break;
+		case "team":
+		System.out.println("open text");
+		break;
+		case "editor":
+		System.out.println("ajay & pavan");
+		break;
+		default:
+		System.out.println("Invalid entry");
+		break;
+
+	}
+
+	}
+}
