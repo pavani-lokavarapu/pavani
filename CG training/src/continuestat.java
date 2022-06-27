a6064d7f6ccb53ad15d9e473e50b7bed77f47526
acer <acer@192.168.0.100>
2022-06-27 14:25:29 +0530

    continue

---
+++ CG training/src/continuestat.java
@@ -0,0 +1,15 @@
+
+public class continuestat {
+
+	public static void main(String[] args) {
+		for(int i=1; i<20; i++)
+		 {
+		 if(i == 1)
+		 {
+		 continue; 
+		 }
+		 System.out.println(i);
+	}
+
+}
+}
