1ade7c665a353ee6ee73b4d33d16a5e30764bdd4
acer <acer@192.168.0.102>
2022-06-29 12:53:59 +0530

    display values

---
+++ CG training/src/com/SVKPCG/scannerreader/demoscanner.java
@@ -0,0 +1,16 @@
+package com.SVKPCG.scannerreader;
+
+import java.util.Scanner;
+
+public class demoscanner {
+
+	public static void main(String[] args) {
+		System.out.println("enter a data");
+		Scanner obj=new Scanner(System.in);
+		 int x;
+		x=obj.nextInt();
+		System.out.println("The entered number is:"+x);
+
+	}
+
+}
