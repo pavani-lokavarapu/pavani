4da2d78da654eb387550e5b527b671491dfdcc28
acer <acer@TMP249-G2-M.bbrouter>
2022-07-15 11:45:53 +0530

    multiplicationtable

---
+++ CG training/src/assignment/multiplicationtable.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class multiplicationtable {
+
+	public static void main(String[] args) {
+		int num = 20;
+		for(int i = 1; i<=10; ++i) {
+			System.out.printf("%d *%d = %d \n",num,i,num*i);
+		}
+
+	}
+
+}
