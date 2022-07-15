2088373842fa828d0cfc48560fa8f277ad3dfdc0
acer <acer@TMP249-G2-M.bbrouter>
2022-07-15 12:07:12 +0530

    month name

---
+++ CG training/src/assignment/monthdays.java
@@ -0,0 +1,51 @@
+package assignment;
+
+public class monthdays {
+
+	public static void main(String[] args) {
+	int n=8;
+	switch(n)
+	{
+	case 1:
+		System.out.println("january has 31 days");
+	break;
+	case 2:
+		System.out.println("february has 28 days");
+	break;
+	case 3:
+		System.out.println("march has 31 days");
+	break;
+	case 4:
+		System.out.println("april has 30 days");
+	break;
+	case 5:
+		System.out.println("may has 31 days");
+	break;
+	case 6:
+		System.out.println("june has 30 days");
+	break;
+	case 7:
+		System.out.println("july has 31 days");
+	break;
+	case 8:
+		System.out.println("august has 31 days");
+	break;
+	case 9:
+		System.out.println("september has 30 days");
+	break;
+	case 10:
+		System.out.println("october has 31 days");
+	break;
+	case 11:
+		System.out.println("november has 31 days");
+	break;
+	case 12:
+		System.out.println("december has 31 days");
+	break;
+	default:
+		System.out.println("invalid number");
+	
+	}
+}
+
+}
