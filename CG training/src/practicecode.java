1d0f18b211be3e0db63cf5d055ee120415dad4a5
pavani <pavani@LAPTOP-8DFTNI5T>
2022-08-07 17:26:00 +0530

    result

---
+++ CG training/src/practicecode.java
@@ -0,0 +1,26 @@
+
+public class practicecode {
+   //  static float result;
+	public static void main(String[] args) {
+	
+	//	result=5/2;
+	//	System.out.println(result);
+ // for(int i=0;i<=10;i++) {
+//	if(i>6)
+	//	break;
+//}
+  //int i=5;
+  // System.out.println(i);
+		//for(int i=0;i<=0;i++) {
+			//for(int j=2;j>=0;j--) {
+				//if(i==j)break;
+			//System.out.println("i="+ i + "j=" +j);
+			for(int i=1,j=1; i<10||j<10;i++,j++)
+			 { 
+			 j++;
+			 System.out.println("i="+i+" and j="+j); 
+		}
+
+}
+	}
+
