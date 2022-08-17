007072810431499bf10d9c53482209fae20eee97
pavani <pavani@LAPTOP-8DFTNI5T.bbrouter>
2022-08-17 10:43:01 +0530

    client application

---
+++ CG training/src/com/svkp/Client/Client.java
@@ -0,0 +1,22 @@
+package com.svkp.Client;
+
+import com.svkp.bankAppilication.GSSavingAcc;
+import com.svkp.bankAppilication.GsBankFactory;
+import com.svkp.bankAppilication.GsCurrentAcc;
+
+public class Client {
+
+	
+	public static void main(String[] args) {
+		GsBankFactory gsbankfac= new GsBankFactory(0, null, 0, false);
+		gsbankfac.getNewSavingAcc(0, null, 0, false);
+		gsbankfac.getNewCurrentAcc(123, "suraj", 23456, true);
+		
+		GSSavingAcc gssaving = new GSSavingAcc(125,"krishna",123454,true);
+		gssaving.withdrow(12344);
+		
+		GsCurrentAcc gscurrent = new GsCurrentAcc(234,"ram",56478,true);
+		gscurrent.diposit(123);
+	}
+
+}
